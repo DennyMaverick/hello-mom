51b208167f9f3e29d0a9d713447ed97c9cc9bf8e
Denis Mamlev <81098489+DennyMaverick@users.noreply.github.com>
2024-07-15 16:00:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.message__btn');
+
+const output = document.querySelector('.message__output');
+
+btn.addEventListener('click', function () {
+  output.style.opacity = 1;
+})
